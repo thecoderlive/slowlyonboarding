c56bd9760f76dd58f6e7b04a9771966b187e4f71
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-08 11:40:21 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,6 @@
+export const item = {
+    "id": "4d10e3a2-226c-4f99-8b46-ff0a5777bfa5",
+    "onboarding_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/277.png",
+    "notification_title": "clearly",
+    "about_notification": "This country sort state past as. Begin anything sport role morning result."
+}
